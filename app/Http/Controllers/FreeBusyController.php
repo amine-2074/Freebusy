a2ae687dd52cdf376b731fd838da7bcfe89a11b1
Amine Mejri <aminemejri1997@gmail.com>
2022-12-26 09:05:38 +0100

    create free busy controller and declare the route with a test function

---
+++ app/Http/Controllers/FreeBusyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Freebusy\Filter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class FreeBusyController extends Controller
+{
+    public function getData()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FreeBusyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getData', [FreeBusyController::class, 'getData'])->name('getData');
