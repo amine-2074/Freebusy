f7e8147684dd5ead33e4dfe7f8fc42251bf922de
Amine Mejri <aminemejri1997@gmail.com>
2023-01-11 16:30:29 +0100

    get busy dates by day

--- app/Freebusy/Meeting.php
+++ app/Freebusy/Meeting.php
@@ -16,19 +16,43 @@ class Meeting
         $earliest_requested_date = date('Y-m-d H:i:s', strtotime($data['earliest_requested_date']));
         $latest_requested_date = date('Y-m-d H:i:s', strtotime($data['latest_requested_date']));
         $participant_id = $data['participants'][0];
-        $busy_dates = Freebusy::where('employee_id' , $participant_id)->get();
+        $busy_dates = Freebusy::where('employee_id', $participant_id)->get();
         $day_busy_date = [];
 
         //put all busy dates in the requested period period in one array
-        foreach($busy_dates as $busy)
-        {
-            if(strtotime($busy->start_busy) >= strtotime($earliest_requested_date) && strtotime($busy->end_busy) <= strtotime($latest_requested_date))
-            $day_busy_date[] = [
-                'start_busy' =>$busy->start_busy->format('Y-m-d H:i:s'),
-                'end_busy' =>$busy->end_busy->format('Y-m-d H:i:s')
-            ];
+        foreach ($busy_dates as $busy) {
+            if (strtotime($busy->start_busy) >= strtotime($earliest_requested_date) && strtotime($busy->end_busy) <= strtotime($latest_requested_date)) {
+                $day_busy_date[] = [
+                    'start_busy' =>$busy->start_busy->format('Y-m-d H:i:s'),
+                    'end_busy' =>$busy->end_busy->format('Y-m-d H:i:s')
+                ];
+            }
         }
-        
-        dd(collect($day_busy_date)->sortBy('start_busy')->all());
+        // dd(collect($day_busy_date)->sortBy('start_busy'));
+
+        $collection = collect($day_busy_date)->sortBy('start_busy');
+        // dd($collection);
+        $busyByDay = [];
+        foreach ($collection as $busy) {
+            $date = date("Y-m-d", strtotime($busy['start_busy']));
+            $filteredDay = $collection->filter(function ($item) use ($date) {
+                return date("Y-m-d", strtotime($item['start_busy'])) == $date;
+            });
+            foreach ($filteredDay as $key=>$datetime) {
+                $day = date('Y-m-d', strtotime($filteredDay[$key]['start_busy']));
+                $busyByDay[$day][] = $datetime;
+            }
+        }
+        foreach ($busyByDay as $day=>$value) {
+            dd($value);
+            $result = array_reduce($busyByDay[$day], function ($carry, $item) {
+                $key = $item['start_busy'].$item['end_busy'];
+                if (!isset($carry[$key])) {
+                    $carry[$key] = $item;
+                }
+                return $carry;
+            }, array());
+        }
+        dd($result);
     }
 }
