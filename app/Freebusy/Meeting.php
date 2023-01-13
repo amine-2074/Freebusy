c458177907d188ad29731ef7a8d481de819fb7a9
Amine Mejri <aminemejri1997@gmail.com>
2023-01-13 15:00:47 +0100

    get the full period between start requested and end requested

--- app/Freebusy/Meeting.php
+++ app/Freebusy/Meeting.php
@@ -17,19 +17,18 @@ class Meeting
         $latest_requested_date = date('Y-m-d H:i:s', strtotime($data['latest_requested_date']));
         $participants = $data['participants'];
         $free_datetime = [];
-        foreach($participants as $participant_id)
-        {
+        foreach ($participants as $participant_id) {
             $busy_dates = Freebusy::where('employee_id', $participant_id)->get();
             $day_busy_date = [];
 
             //put all busy dates in the requested period period in one array
             foreach ($busy_dates as $busy) {
-            if (strtotime($busy->start_busy) >= strtotime($earliest_requested_date) && strtotime($busy->end_busy) <= strtotime($latest_requested_date)) {
-                $day_busy_date[] = [
-                    'start_busy' =>$busy->start_busy->format('Y-m-d H:i:s'),
-                    'end_busy' =>$busy->end_busy->format('Y-m-d H:i:s')
-                ];
-            }
+                if (strtotime($busy->start_busy) >= strtotime($earliest_requested_date) && strtotime($busy->end_busy) <= strtotime($latest_requested_date)) {
+                    $day_busy_date[] = [
+                        'start_busy' =>$busy->start_busy->format('Y-m-d H:i:s'),
+                        'end_busy' =>$busy->end_busy->format('Y-m-d H:i:s')
+                    ];
+                }
             }
             //collect busy datetimes by date
             $collection = collect($day_busy_date)->sortBy('start_busy');
@@ -56,51 +55,55 @@ class Meeting
                     if (!isset($carry[$key])) {
                         // If it does not exist, add the current item to the $carry array with the key created previously
                         $carry[$key] = $item;
-                }
-                return $carry;
-            }, array());
-            // dd($busyByDay[$day]);
-            $busyByDay[$day] = array_values($result);
+                    }
+                    return $carry;
+                }, array());
+                // dd($busyByDay[$day]);
+                $busyByDay[$day] = array_values($result);
             }
             $freetime = [];
             foreach ($busyByDay as $date => $intervals) {
-            $date_start = Carbon::parse($date)->startOfDay()->addHours($data['office_hours_start'])->format('Y-m-d H:i:s');
-            $date_end = Carbon::parse($date)->startOfDay()->addHours($data['office_hours_end'])->format('Y-m-d H:i:s');
-            // Initialize an array to store the free intervals for the current date
-            $busy = [];
-            // Check if there is any busy intervals for the current date
-            if(count($intervals) > 0){
-                // Check if the first busy interval starts after the office start time
-                if(strtotime($intervals[0]['start_busy']) > strtotime($date_start)){
-                    $busy[] = [$date_start, $intervals[0]['start_busy']];
+                $days = [];
+                $date_start = Carbon::parse($date)->startOfDay()->addHours($data['office_hours_start'])->format('Y-m-d H:i:s');
+                $date_end = Carbon::parse($date)->startOfDay()->addHours($data['office_hours_end'])->format('Y-m-d H:i:s');
+                // Initialize an array to store the free intervals for the current date
+                $busy = [];
+                // Check if there is any busy intervals for the current date
+                if (count($intervals) > 0) {
+                    // Check if the first busy interval starts after the office start time
+                    if (strtotime($intervals[0]['start_busy']) > strtotime($date_start)) {
+                        $busy[] = [$date_start, $intervals[0]['start_busy']];
+                    }
+                    // Iterate through the busy intervals for the current date
+                    for ($i = 0; $i < count($intervals) - 1; $i++) {
+                        // Add the free interval between the current busy interval and the next busy interval
+                        $busy[] = [$intervals[$i]['end_busy'], $intervals[$i + 1]['start_busy']];
+                    }
+                    // Check if the last busy interval ends before the office end time
+                    if (strtotime($intervals[count($intervals) - 1]['end_busy']) < strtotime($date_end)) {
+                        $busy[] = [$intervals[count($intervals) - 1]['end_busy'], $date_end];
+                    }
+                } else {
+                    // If no busy intervals, the whole day is free
+                    $busy[] = [$date_start, $date_end];
                 }
-                // Iterate through the busy intervals for the current date
-                for ($i = 0; $i < count($intervals) - 1; $i++) {
-                    // Add the free interval between the current busy interval and the next busy interval
-                    $busy[] = [$intervals[$i]['end_busy'], $intervals[$i + 1]['start_busy']];
-                }
-                // Check if the last busy interval ends before the office end time
-                if(strtotime($intervals[count($intervals) - 1]['end_busy']) < strtotime($date_end)){
-                    $busy[] = [$intervals[count($intervals) - 1]['end_busy'], $date_end];
-                }
-            }else{
-                // If no busy intervals, the whole day is free
-                $busy[] = [$date_start, $date_end];
+                // Add the free intervals for the current date to the free datetime array
+                $free_datetime[$participant_id][$date] = $busy;
             }
-            // Add the free intervals for the current date to the free datetime array
-            $free_datetime[$participant_id][$date] = $busy;
+            $start = Carbon::createFromFormat('Y-m-d H:i:s', $earliest_requested_date);
+            $end = Carbon::createFromFormat('Y-m-d H:i:s', $latest_requested_date);
+            $days = $start->diffInDays($end);
+            $dateRange = array();
+            for ($i = 0; $i <= $days; $i++) {
+                $dateRange[$start->copy()->addDays($i)->format('Y-m-d')] = [
+                    0 => $start->copy()->addDays($i)->startOfDay()->addHours($data['office_hours_start'])->format('Y-m-d H:i:s'),
+                    1 => $start->copy()->addDays($i)->startOfDay()->addHours($data['office_hours_end'])->format('Y-m-d H:i:s')
+                ];
             }
+            $freedays = array_diff_key($dateRange, $free_datetime[$participant_id]);
+            $totalfree = array_merge($freedays, $free_datetime[$participant_id]);
+            dd($totalfree);
         }
         dd($free_datetime);
-    }
-    public function getFreeIntervals($start, $end, $busy)
-    {
-        sort($busy);
-        $free = [[$start, $busy[0][0]]];
-        for ($i = 0; $i < count($busy) - 1; $i++) {
-            $free[] = [$busy[$i][1], $busy[$i + 1][0]];
-        }
-        $free[] = [$busy[count($busy) - 1][1], $end];
-        return $free;
     }
 }
